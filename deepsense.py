1baec46c8c1056a31a78dc00b490802684ab3c55
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-03-02 22:56:47 +0800

    代码有修改！

---
+++ deepsense.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2018-2021, Node Supply Chain Manager Corporation Limited.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: deepsense
+@time: 2019/3/2 22:54
+@desc:
+'''
+import tensorflow as tf
+import numpy as np
+from tensorflow_own.AllNet import CNN, RNN, FNN
