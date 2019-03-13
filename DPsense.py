4edacc8a6b005a6379903b576faa36b962d498f3
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-03-13 22:47:42 +0800

    代码有修改

--- DPsense.py
+++ DPsense.py
@@ -64,8 +64,8 @@ def deepsense_model():
             fileoperation = FileoOperation(
                 ftype= tf.float64,#tf.float64
                 ttype= tf.float64,
-                fshape= (18000),
-                tshape= (6),
+                fshape= [18000],
+                tshape= [6],
                 batch_size= BATCH_SIZE,
                 capacity= 600+60*50,
                 batch_fun= 'shuffle'
@@ -120,7 +120,7 @@ def deepsense_model():
             cnn_gry_1 = CNN(x=None, w_conv=conv_para_1['w1'], stride_conv=None,
                             stride_pool=None)  # x:(batch_size*T, 3*2*F)
             input_gry_reshape = cnn_gry_1.reshape(f_vector=input_gry, new_shape=(-1, 3, 2 * F, 1))
-            conv1_gry = sub_conv_bn_dropout(CNN_object=cnn_gry_1, op=input_gry, is_training=is_training,
+            conv1_gry = sub_conv_bn_dropout(CNN_object=cnn_gry_1, op=input_gry_reshape, is_training=is_training,
                                             strides=[1, 1, CONV_LEN_1, 1])  # (batch_size*T, :, :, OUT_NUM)
 
             cnn_gry_2 = CNN(x=None, w_conv=conv_para_1['w2'], stride_conv=None,
@@ -162,7 +162,8 @@ def deepsense_model():
             # conv3_mag_fin = tf.expand_dims(input=conv3_mag_flat, axis=1)
             cnn_concat1 = tf.concat([conv3_acc_fin, conv3_gry_fin], axis=1)
             # cnn_concat1 = tf.concat([conv3_acc_fin, conv3_gry_fin, conv3_mag_fin], axis=1)
-            print('第一部分卷积层输出单个批次维度为: %s' % cnn_concat1.shape) #shape=(batch_size*T, 2(3), :)
+            cnn_concat1 = tf.expand_dims(input=cnn_concat1, axis=-1)
+            print('第一部分卷积层输出单个批次维度为: %s' % cnn_concat1.shape) #shape=(batch_size*T, 2(3), :, 1)
 
         # 第二部分卷积层（结构和第一部分每个传感器的卷积层结构相似）
         with tf.name_scope('cnn_part2'):
@@ -182,9 +183,9 @@ def deepsense_model():
                 summary_visalization.variable_summaries(var=conv_para_2['w2'], name='part2_w2')
                 summary_visalization.variable_summaries(var=conv_para_2['w3'], name='part2_w3')
 
-            cnn_merge_1 = CNN(x=cnn_concat1, w_conv=conv_para_2['w1'], stride_conv=None,
+            cnn_merge_1 = CNN(x=None, w_conv=conv_para_2['w1'], stride_conv=None,
                               stride_pool=None)  # x:(batch_size*T, 2(3), :)
-            conv1_merge = sub_conv_bn_dropout(CNN_object=cnn_merge_1, op=cnn_merge_1, is_training=is_training,
+            conv1_merge = sub_conv_bn_dropout(CNN_object=cnn_merge_1, op=cnn_concat1, is_training=is_training,
                                      strides=[1, 1, CONV_MEG_1, 1])  # (batch_size*T, :, :, OUT_NUM)
 
             cnn_merge_2 = CNN(x=None, w_conv=conv_para_2['w2'], stride_conv=None,
@@ -200,8 +201,77 @@ def deepsense_model():
             #对卷积层的输出在后三个维度上进行flat并组合WIDTH参数在各个特征向量的最后
             axis_cnn = cnn_fin.get_shape().as_list()
             cnn_flat = tf.reshape(tensor=cnn_fin, shape=(-1, axis_cnn[1]*axis_cnn[2]*axis_cnn[3])) #(batch_size*T, :)
-            const_WIDTH = tf.constant(value=np.ones(shape=(axis_cnn[0], 1)*WIDTH), dtype= tf.float32)
+            const_WIDTH = tf.constant(value=np.ones(shape=(cnn_flat.get_shape().as_list()[0], 1)*WIDTH), dtype= tf.float32)
             cnn_WIDTH = tf.concat([cnn_flat, const_WIDTH], axis=1)
             print('组合WIDTH后的维度为: %s' % cnn_WIDTH.shape)
+        with tf.name_scope('rnn'):
+            #对第二部分卷积输出做维度转换，转换后为:(batch_size, T, :)
+            axis_cnn_WIDTH = cnn_WIDTH.get_shape().as_list()
+            cnn_WIDTH_flat = tf.reshape(tensor=cnn_WIDTH, shape=(1, axis_cnn_WIDTH[0]*axis_cnn_WIDTH[1]))
+            cnn_output = tf.reshape(tensor=cnn_WIDTH_flat, shape=(-1, T*axis_cnn_WIDTH[1])) #(batch_size, T*:)
+            cnn_output = tf.reshape(tensor=cnn_output, shape=(-1, axis_cnn_WIDTH, T)) #(batch_size, :, T)
+            rnn_input = tf.transpose(a=cnn_output, perm=[0, 2, 1]) #(batch_size, T, :)
+            # multiGRU部分
+            gru = RNN(x=rnn_input, max_time=T, num_units=128)
+            rnn_outputs, _ = gru.dynamic_multirnn(style='GRU', layers_num=2, output_keep_prob=0.8)  # rnn_outputs:(batch_size, T, num_units)
+            # print(rnn_outputs.get_shape().as_list())
+            # 将T个时刻的所有输出求平均值
+            rnn_output = tf.reduce_mean(rnn_outputs, axis=1)  # rnn_output:(batch_size, num_units)
+            # print(rnn_output.get_shape().as_list())
+        with tf.name_scope('output_layer'):
+            w_size_in = rnn_outputs.get_shape().as_list()[-1]
+            w_size_out = 6
+            w_out = tf.Variable(tf.truncated_normal(shape=(w_size_in, w_size_out), mean=0, stddev=1), dtype=tf.float32)
+            b = tf.Variable(tf.truncated_normal(shape=([w_size_out]), mean=0, stddev=1), dtype=tf.float32)
+            summary_visalization.variable_summaries(var=w_out, name='fc_w')
+            summary_visalization.variable_summaries(var=b, name='fc_b')
+            fin_output = tf.matmul(rnn_output, w_out) + b
+        with tf.name_scope('evaluation'):
+            # 定义softmax交叉熵和损失函数以及精确度函数
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=fin_output, labels=label_batch))
+            # 添加摘要loss
+            summary_visalization.scalar_summaries(arg={'loss': loss})
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-2).minimize(loss)
+            evaluation = Evaluation(one_hot=True, logit=tf.nn.softmax(fin_output), label=label_batch, regression_pred=None,
+                                    regression_label=None)
+            acc = evaluation.acc_classification()
+        with tf.name_scope('etc'):
+            merge = summary_visalization.summary_merge()
+            init_global = tf.global_variables_initializer()
+            init_local = tf.local_variables_initializer()
+            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options), graph=ds_graph) as sess:
+        sess.run(init_global)
+        sess.run(init_local)
+        # 摘要文件
+        summary_writer = summary_visalization.summary_file(p='logs/', graph=sess.graph)
+        # 线程调配管理器
+        coord = tf.train.Coordinator()
+        # Starts all queue runners collected in the graph.
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        train_steps = TRAIN_STEPS
+        try:
+            while not coord.should_stop():  # 如果线程应该停止则返回True
+                # feature_batch_, target_batch_ = sess.run([feature_batch, label_batch])
+                # print(feature_batch_.shape, target_batch_.shape)
+                summary = sess.run(merge, feed_dict={is_training: False})
+                _ = sess.run(optimizer, feed_dict={is_training: True})
+                train_steps -= 1
+                if train_steps <= 0:
+                    coord.request_stop()  # 请求该线程停止，若执行则使得coord.should_stop()函数返回True
+                if (TRAIN_STEPS - train_steps) % (12 * 100) == 0:  # 读入批次总数
+                    acc_ = sess.run(acc, feed_dict={is_training: False})
+                    print(acc_)
+                summary_visalization.add_summary(summary_writer=summary_writer, summary=summary,
+                                                 summary_information=(TRAIN_STEPS - train_steps + 1))
+        except tf.errors.OutOfRangeError:
+            print('%s次训练完成' % (TRAIN_STEPS // 12))
+        finally:
+            # When done, ask the threads to stop. 请求该线程停止
+            coord.request_stop()
+            # And wait for them to actually do it. 等待被指定的线程终止
+            coord.join(threads)
+            summary_visalization.summary_close(summary_writer=summary_writer)
 
-
+if __name__ == '__main__':
+    deepsense_model()
